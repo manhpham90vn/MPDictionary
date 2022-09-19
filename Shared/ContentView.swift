a51996d817db9f0964b9e114e1e385720bbea172
Manh Pham <manhpham90vn@icloud.com>
2022-09-19 15:38:41 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Manh Pham on 19/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MPDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPDictionaryApp.swift
+//  Shared
+//
+//  Created by Manh Pham on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MPDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
